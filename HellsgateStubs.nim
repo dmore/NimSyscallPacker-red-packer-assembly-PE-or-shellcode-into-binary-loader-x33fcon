ff9cbc33828600b9ebc7522836a0c8335f661f18
S3cur3Th1sSh1t <S3cur3Th1sSh1t@github.com>
2022-06-20 21:36:24 +0200

    Fix startprocess for --hellsgate

--- HellsgateStubs.nim
+++ HellsgateStubs.nim
@@ -915,6 +915,8 @@ when isMainModule:
 
 let HellsgateNotepadProcIDStub * = """
 
+import osproc
+
 # Under the hood, the startProcess function from Nim's osproc module is calling CreateProcess() :D
 let tProcess = startProcess(obf("notepad.exe"))
 tProcess.suspend() # That's handy!
