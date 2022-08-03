35ad8fcf20395ef09fa9d885a35e4f2cd7513362
S3cur3Th1sSh1t <S3cur3Th1sSh1t@github.com>
2022-08-03 14:24:33 +0200

    Comments with new features

--- AntiDebug.nim
+++ AntiDebug.nim
@@ -51,3 +51,58 @@ proc AmIDebugged*(): bool =
       return true
     else:
       return false
+
+#[
+   More to come - https://0xpat.github.io/Malware_development_part_3/
+
+   #define FLG_HEAP_ENABLE_TAIL_CHECK   0x10
+   #define FLG_HEAP_ENABLE_FREE_CHECK   0x20
+   #define FLG_HEAP_VALIDATE_PARAMETERS 0x40
+   #define NT_GLOBAL_FLAG_DEBUGGED (FLG_HEAP_ENABLE_TAIL_CHECK | FLG_HEAP_ENABLE_FREE_CHECK | FLG_HEAP_VALIDATE_PARAMETERS)
+   PDWORD pNtGlobalFlag = (PDWORD)(__readgsqword(0x60) + 0xBC);
+   if ((*pNtGlobalFlag) & NT_GLOBAL_FLAG_DEBUGGED) return false;
+
+
+and
+
+
+PDWORD pHeapFlags = (PDWORD)((PBYTE)GetProcessHeap() + 0x70);
+PDWORD pHeapForceFlags = (PDWORD)((PBYTE)GetProcessHeap() + 0x74);
+if (*pHeapFlags ^ HEAP_GROWABLE || *pHeapForceFlags != 0) return false;
+
++
+
+CONTEXT context = {};
+context.ContextFlags = CONTEXT_DEBUG_REGISTERS;
+GetThreadContext(GetCurrentThread(), &context);
+if (context.Dr0 || context.Dr1 || context.Dr2 || context.Dr3) return;
+
+or
+
+
+BOOL isDebugged = TRUE;
+
+LONG WINAPI CustomUnhandledExceptionFilter(PEXCEPTION_POINTERS pExceptionPointers)
+{
+	isDebugged = FALSE;
+	return EXCEPTION_CONTINUE_EXECUTION;
+}
+
+void main()
+{
+	PTOP_LEVEL_EXCEPTION_FILTER previousUnhandledExceptionFilter = SetUnhandledExceptionFilter(CustomUnhandledExceptionFilter);
+	RaiseException(EXCEPTION_FLT_DIVIDE_BY_ZERO, 0, 0, NULL);
+	SetUnhandledExceptionFilter(previousUnhandledExceptionFilter);
+	if (isDebugged) return;
+
+	wprintf_s(L"Now hacking...\n");
+}
+
+Hide Thread from a debugger:
+
+typedef NTSTATUS(WINAPI *NtSetInformationThread)(IN HANDLE, IN THREADINFOCLASS, IN PVOID, IN ULONG);
+NtSetInformationThread pNtSetInformationThread = (NtSetInformationThread)GetProcAddress(GetModuleHandleW(L"ntdll.dll"), "NtSetInformationThread");
+THREADINFOCLASS ThreadHideFromDebugger = (THREADINFOCLASS)0x11;
+pNtSetInformationThread(GetCurrentThread(), ThreadHideFromDebugger, NULL, 0);
+
+]#
